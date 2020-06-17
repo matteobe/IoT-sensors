6d1b1e8df8babeba8155a5ae7a6cc539a1d59f74
matteobe <maberchier@gmail.com>
2020-06-17 14:27:35 +0200

    Implementation of sockets, to avoid socket closing error, tested with 100 data points

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 # Guides:
 # http://docs.micropython.org/en/v1.8.7/esp8266/esp8266/tutorial/network_basics.html
 # https://docs.influxdata.com/influxdb/v1.8/guides/write_data/
+#
+# Python libraries
+# https://makeblock-micropython-api.readthedocs.io/en/latest/library/usocket.html
+# http://docs.micropython.org/en/v1.8.7/esp8266/esp8266/tutorial/network_tcp.html
 
 # Python libraries
 import network
-import urequests as requests
+import usocket as socket
 import time
 
 # M5stack libraries
@@ -41,8 +45,15 @@ else:
 FirstLine.setText(wifi)
 
 ############################# Server Setup ####################################
-# Declare url with protocol and host
-InfluxDB_url = "http://3.14.28.95:8086/write?db=m5stack"
+# Declare the server characteristics
+server_protocol = "http"
+server_host = "3.14.28.95"
+server_port = "8086"
+server_query = "write?db=m5stack"
+
+# Create a socket that can be used to connect and send data
+sock_ai = socket.getaddrinfo(server_host,server_port,0,socket.SOCK_STREAM)
+sock_ai = sock_ai[0]
 
 ############################# Sensors Setup ###################################
 # Retrieve hat environment on M5stack
@@ -58,21 +69,48 @@ printout_template = "T:{},H:{},P:{}"
 ############################# Display Setup ###################################
 # Text box to display data
 SecondLine = M5TextBox(55,10,"",lcd.FONT_Default,0xFFFFFF,rotate=90)
+ThirdLine = M5TextBox(35,10,"",lcd.FONT_Default,0xFFFFFF,rotate=90)
 
 ############################# Measure and stream ##############################
+# Define maximum number of points and counter
+points_max = 100
+points_current = 0
+
+# Tell the user the sensor will start sending now
+FirstLine.setText("Sensor sending...")
+
 # Retrieve sensor data and send to server, display data on device as well
-while True:
+while points_current < points_max:
+    # Send new datapoint
+    points_current += 1
+
     # Sensor data
     temperature = hat_env.temperature
     humidity = hat_env.humidity
     pressure = hat_env.pressure
 
-    # Send data to server
+    # Build data string to be sent to server
     sensor_data = sensor_data_template.format(temperature,humidity,pressure)
-    data_post = requests.post(InfluxDB_url,data=sensor_data)
 
-    # Update text on device
-    SecondLine.setText(printout_template.format(temperature,humidity,pressure))
+    # Connect to server, send data and close socket
+    try:
+        sock = socket.socket(sock_ai[0],sock_ai[1],sock_ai[2])
+        sock.connect(sock_ai[-1])
+        sock.write(b"%s /%s HTTP/1.0\r\n" % ("POST",server_query))
+        sock.write(b"Content-Length: %d" % len(sensor_data))
+        sock.write(sensor_data)
+        sock.close()
+    except:
+        sock.close()
+
+    # Update texts on device
+    SecondLine.setText("Current point: {}".format(points_current))
+    ThirdLine.setText(printout_template.format(temperature,humidity,pressure))
 
     # Wait before repeating cycle
     wait_ms(2000)
+
+# Tell user the sensor has stopped sending after the agreed number of points
+FirstLine.setText("Sensor sent {} points".format(points_max))
+SecondLine.setText("Program finished")
+ThirdLine.setText("")
